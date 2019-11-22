9fcf2d334322e4628e9a439feb6c731a28bd5364
Anton <cf2nehyb$xtu>
2019-11-22 17:56:52 +0500

    Настройки БД

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=127.0.0.2;dbname=apple-test',
+            'dsn' => 'mysql:host=127.0.0.2;port=3307;dbname=apple-test',
             'username' => 'root',
             'password' => 'root',
             'charset' => 'utf8',
